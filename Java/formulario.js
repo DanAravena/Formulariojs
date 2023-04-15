a603815a2bdb254a70458f35683f84e820d696e4
DanAravena <127603013+DanAravena@users.noreply.github.com>
2023-04-15 10:41:14 -0400

    first commit

--- Java/formulario.js
+++ Java/formulario.js
@@ -2,6 +2,7 @@ var nombre = document.getElementById('nombre');
 var rut  =  document.getElementById('rut');
 var apPaterno  =  document.getElementById('apPaterno');
 var apMaterno  =  document.getElementById('apMaterno');
+var edad = document.getElementById('edad')
 var error = document.getElementById('error');
 error.style.color = 'red';
 
@@ -12,19 +13,34 @@ function enviarFormulario(){
     if(nombre.value === null || nombre.value === ''){
         mensajeerror.push('Ingresa tu nombre');
     }
-    if(password.value === null || password.value === ''){
-            mensajeerror.push('Ingresa tu password');
-    error.innerHTML = mensajeerror.join(', ');
-
-    }
     if(rut.value === null || rut.value === ''){
         mensajeerror.push('Ingresa tu Rut');
     }
     if(apPaterno.value === null || apPaterno.value === ''){
         mensajeerror.push('Ingresa tu Apellido Paterno');
-    }    
+    } 
+    if (edad==""){
+        alert("Tiene que introducir un número entero en su edad.")
+        document.fvalida.edad.focus()
+        return 0;
+     }
+     else{
+        if (edad<18){
+               alert("Debe ser mayor de 18 años.")
+               document.fvalida.edad.focus()
+               return 0;
+        }
+        if (edad>35){
+            alert("Debe ser Menor de 35 años.")
+            document.fvalida.edad.focus()
+            return 0;
+     }
+ }
+    
     if(apMaterno.value === null || apMaterno.value === ''){
         mensajeerror.push('Ingresa tu Apellido Materno');
-    }  
+        error.innerHTML = mensajeerror.join(', ');
+    }
+      
     return  false;
 }
